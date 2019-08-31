4f935f476b164041cbdc247224df32f8fa7c2a3c
Bigfoot <yume7168@naver.com>
2019-08-31 11:40:20 +0900

    1. 좀 더 어두운 화면으로 콘솔 느낌을 연출하고 싶어서 css 조정
    TODO (new)
    - DB설계
    - 게시판 페이지 프레임 만들기
    - 블로그 페이지 프레임 만들기
    - (side Project) QRcode 생성기
    - NAS 만들기
    - filter grayscale 적용
    - es6 호환 가능하게 babel사용
    
    TODO List
    - ajax DB연결
    - pushstate
    - 깃헙 포트폴리오용 소개 할 것들 조사하여 정리
    - 나머지 DB관련은 다른 프로젝트에서 진행 후 같이 연결 예정
    - 모달창, aside태그(드래그앤드랍) 모바일터치 지원

--- 2_js/ContentsScript.js
+++ 2_js/ContentsScript.js
@@ -20,19 +20,13 @@ function wrapContents_init() {
     // <img src="/4_img/liquor_Trim.gif" width=90% >
     `
     <p> 여기는 컨텐츠입니다 임시로 할당 받았습니다</p>
-
+    <img src="/4_img/Tesla_Sarony.jpg">
     <p> 이미지도 한번 넣어봤습니다</p>
     <p> 템플릿 리터럴은 정말 좋은 것 같습니다.</p>
     <p> 근데 이 문자(\`)의 이름이 뭔가요?</p>
     <p> 그레이브 엑센트라고도 하고</p>
     <p> 백틱이라고도 하던데 어느게 맞는지 모르겠습맞는지 모르겠습맞는지 모르겠습니다</p>
     <p> 둘 다 맞는 것일까요?</p>
-    <BR>
-    <BR>
-    <BR>
-    <BR>
-    <BR>
-    <BR>
     <BR>
     <BR>
     <BR>
