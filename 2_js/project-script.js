a103523759191490ec7aa8492344ec1081573e33
bigfoot7774 <yume7168@naver.com>
2020-09-13 09:04:37 +0900

    프로젝트 썸네일 추가 및 그에 관련된 스타일 조정

--- 2_js/project-script.js
+++ 2_js/project-script.js
@@ -10,25 +10,34 @@ function ShowSelectedContents(data) {
     window.scrollTo(0, 0);
 }
 
-function makeprojectSection(data) {
+function makeProjectSection(data) {
     var projectSectionContents = document.querySelector('#project-section-contents');
     var parsedData = JSON.parse(data);
     for (var parsedDataKey in parsedData) {
         var tagSection = document.createElement('section');
         var tagH2 = document.createElement('h2');
         var tagH5 = document.createElement('h5');
+        var tagThumbnailDiv = document.createElement('div');
         var tagOutDiv = document.createElement('div');
         var tagInDiv = document.createElement('div');
 
         tagH2.innerHTML = parsedDataKey;
-        tagInDiv.innerHTML = parsedData[parsedDataKey];
+        if (parsedData[parsedDataKey]['thumbnail'] === null || parsedData[parsedDataKey]['thumbnail'] === "") {
+            tagThumbnailDiv.innerHTML = '<img src="4_img/icon/Xasquatch.ico"/>';
+
+        } else {
+            tagThumbnailDiv.innerHTML = parsedData[parsedDataKey]['thumbnail'];
+        }
+        tagInDiv.innerHTML = parsedData[parsedDataKey]['detail'];
         tagH5.innerHTML = 'CLICK TO CLOSE';
 
-        tagH2.setAttribute('align','center');
-        tagH5.setAttribute('align','center');
+        tagH2.setAttribute('align', 'center');
+        tagH5.setAttribute('align', 'center');
+        tagThumbnailDiv.setAttribute('align', 'center')
 
         tagSection.append(tagH2);
         tagSection.append(tagH5);
+        tagOutDiv.append(tagThumbnailDiv);
         tagOutDiv.append(tagInDiv);
         tagSection.append(tagOutDiv);
 
