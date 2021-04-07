b736a165385323d4d96ffa6d0ac4fe30e0d40fcb
xasquatch <bigfoot7774@gmail.com>
2021-04-07 22:13:10 +0900

    - 절대경로와 상대경로차로 인해 404에러가 뜨는 부분 수정(OnePage앱내에서만 상대경로를 절대경로로 재정비할 예정) - 그 외 출력을 위해 바탕을 흰색으로 변경하면서 하얀아이콘이 안보이는 문제가 생겨 검은색 아이콘도 추가하고 변경함

--- js/onePageApp.js
+++ js/onePageApp.js
@@ -1,7 +1,7 @@
 var onePage = {
 
-    squareFull: '<img src="/img/language%20icon/square-full.png" style="width: 1.5em; height: 1.5em; margin: 1px;">',
-    squareBlank: '<img src="/img/language%20icon/square-blank.png" style="width: 1.5em; height: 1.5em; margin: 1px;">',
+    squareFull: '<img src="/img/language%20icon/black-square-full.png" style="width: 1.5em; height: 1.5em; margin: 1px;">',
+    squareBlank: '<img src="/img/language%20icon/black-square-blank.png" style="width: 1.5em; height: 1.5em; margin: 1px;">',
 
     viewProject: function (element, url) {
         request.submit('GET', url, function (data) {
@@ -15,6 +15,92 @@ var onePage = {
             element.remove();
         });
     },
+    createSkill: function (language, imgURL, level, description) {
+        var skillContainer = document.createElement('div');
+        var title = document.createElement('div');
+        var titleImg = document.createElement('img');
+        var skillSet = document.createElement('div');
+        var skillLevel = document.createElement('div');
+        var skillDescription = document.createElement('div');
+
+        skillContainer.id = language + '-stacks-container';
+        skillContainer.className = 'stacks-container';
+        skillContainer.setAttribute('onclick', 'skill.expandDescription(this,"' + description + '");');
+        title.className = 'stacks--title flex';
+        titleImg.src = imgURL;
+        titleImg.style.maxWidth = '60px';
+        titleImg.style.maxHeight = '60px';
+        titleImg.style.padding = '5px';
+        titleImg.setAttribute('onload',
+            'onePage.levelLoading(' + level + ',this.parentNode.parentNode.querySelector(\'#' + language + '\'),' +
+            'this.parentNode.parentNode.querySelector(\'#' + language + 'Description\');' +
+            'skill.expandDescription(this.parentNode,"' + description + '");)');
+        skillSet.className = 'profile-skill-set';
+        skillLevel.id = language;
+        skillDescription.id = language + 'Description';
+        skillDescription.className = 'stacks-description';
+        skillDescription.innerHTML = '<h2 style="margin: 0;">' + language + "</h2>";
+
+        skillContainer.appendChild(title);
+        skillContainer.appendChild(skillSet);
+
+        title.appendChild(titleImg);
+        skillSet.appendChild(skillLevel);
+        skillSet.appendChild(skillDescription);
+
+        return skillContainer;
+    },
+    levelLoading: function (level, tagElement) {
+        var count = 0;
+        var levelPercent = 0;
+        var levelBar = '';
+        var percent;
+        var squareFull = '<img src="/img/language%20icon/black-square-full.png" style="width: 1em; height: 1em; margin: 1px;">';
+        var squareBlank = '<img src="/img/language%20icon/black-square-blank.png" style="width: 1em; height: 1em; margin: 1px;">';
+        var interval = setInterval(function () {
+                try {
+                    if (count < level) {
+                        levelBar += squareFull;
+                        levelPercent += 10;
+                    } else if (count < 10) {
+                        levelBar += squareBlank;
+                    }
+
+                    switch (levelBar) {
+                        case squareFull:
+                            percent = '% (하)';
+                            break;
+                        case squareFull + squareFull + squareFull:
+                            percent = '% (중하)';
+                            break;
+                        case squareFull + squareFull + squareFull + squareFull + squareFull:
+                            percent = '% (중)';
+                            break;
+                        case squareFull + squareFull + squareFull + squareFull + squareFull
+                        + squareFull + squareFull:
+                            percent = '% (중상)';
+                            break;
+                        case squareFull + squareFull + squareFull + squareFull + squareFull
+                        + squareFull + squareFull + squareFull + squareFull:
+                            percent = '% (상)';
+                            break;
+                        case  squareFull + squareFull + squareFull + squareFull + squareFull
+                        + squareFull + squareFull + squareFull + squareFull + squareFull:
+                            percent = '% (최상)';
+                            break;
+                    }
+                    tagElement.innerHTML = 'Level : ' + levelBar + '<BR>' + levelPercent + percent;
+                    count++;
+
+                    if (count === 10) {
+                        clearInterval(interval);
+                    }
+                } catch (error) {
+                    clearInterval(interval);
+                }
+            }, 100
+        );
+    },
     parseSkillList: function (data) {
         var resultElement = document.createElement('div');
         var parsedData = JSON.parse(data);
@@ -26,7 +112,7 @@ var onePage = {
                 var skillLevel = parsedData[key].level;
                 var skillDescription = parsedData[key].description;
 
-                var contents = skill.create(language, img, skillLevel, skillDescription);
+                var contents = onePage.createSkill(language, img, skillLevel, skillDescription);
                 resultElement.appendChild(contents);
             }
         }
@@ -41,9 +127,9 @@ var onePage = {
         var resultLevel = '';
         for (var i = 0; i < 10; i++) {
             if (i < level) {
-                resultLevel += salary.squareFull;
+                resultLevel += onePage.squareFull;
             } else {
-                resultLevel += salary.squareBlank;
+                resultLevel += onePage.squareBlank;
             }
         }
         var levelExplain;
